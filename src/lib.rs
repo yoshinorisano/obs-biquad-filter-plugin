04be4787cd204efcfe1a7f031b297087832ceca3
Yoshinori Sano <yoshinori.sano@gmail.com>
2022-09-18 22:03:48 +0900

    Apply low pass filter from Audio EQ Cookbook

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,55 @@ use obs_wrapper::{
     obs_string,
 };
 
+use std::f32::consts::PI;
+
+struct Coeffs {
+   b0: f32,
+   b1: f32,
+   b2: f32,
+   a1: f32,
+   a2: f32,
+}
+
+struct OldValues {
+    x_n1: f32,
+    x_n2: f32,
+    y_n1: f32,
+    y_n2: f32,
+}
+
 struct BiquadFilter {
-    channels: usize
+    channels: usize,
+    coeffs_low_pass: Coeffs,
+    old_values: [OldValues; 2],
+}
+
+impl BiquadFilter {
+    // Low pass filter from Audio EQ Cookbook.
+    fn create_low_pass(sample_rate: usize, freq: f32, q: f32) -> Coeffs {
+        let w0 = 2.0 * PI * freq as f32 / sample_rate as f32;
+
+        let alpha = w0.sin() / (2.0 * q);
+        let b1 = 1.0 - w0.cos();
+        let b0 = b1 / 2.0;
+        let b2 = b0;
+        let a0 = 1.0 + alpha;
+        let a1 = -2.0 * w0.cos();
+        let a2 = 1.0 - alpha;
+
+        Coeffs {
+            b0: b0 / a0,
+            b1: b1 / a0,
+            b2: b2 / a0,
+            a1: a1 / a0,
+            a2: a2 / a0,
+        }
+    }
+
+    #[inline]
+    fn apply_filter(c: &Coeffs, x_n: f32, x_n1: f32, x_n2: f32, y_n1: f32, y_n2: f32) -> f32 {
+        c.b0 * x_n + c.b1 * x_n1 + c.b2 * x_n2 - c.a1 * y_n1 - c.a2 * y_n2
+    }
 }
 
 impl Sourceable for BiquadFilter {
@@ -19,9 +66,23 @@ impl Sourceable for BiquadFilter {
     }
 
     fn create(create: &mut CreatableSourceContext<Self>, source: SourceContext) -> Self {
-        let channels = create.with_audio(|audio| audio.output_channels());
+        let (sample_rate, channels) =
+            create.with_audio(|audio| (audio.output_sample_rate(), audio.output_channels()));
+        let coeffs_low_pass = BiquadFilter::create_low_pass(sample_rate, 200.0, 0.7);
         Self {
-            channels
+            channels,
+            coeffs_low_pass,
+            old_values: [OldValues {
+                x_n1: 0.0,
+                x_n2: 0.0,
+                y_n1: 0.0,
+                y_n2: 0.0,
+            }, OldValues {
+                x_n1: 0.0,
+                x_n2: 0.0,
+                y_n1: 0.0,
+                y_n2: 0.0,
+            }],
         }
     }
 }
@@ -40,11 +101,24 @@ impl UpdateSource for BiquadFilter {
 
 impl FilterAudioSource for BiquadFilter {
     fn filter_audio(&mut self, audio: &mut audio::AudioDataContext) {
-        let gain = 0.03;
         for channel in 0..self.channels {
             let buffer = audio.get_channel_as_mut_slice(channel).unwrap();
             for output in buffer.iter_mut() {
-                *output = *output * gain;
+                let sample = *output;
+                let result = BiquadFilter::apply_filter(
+                    &self.coeffs_low_pass,
+                    sample,
+                    self.old_values[channel].x_n1,
+                    self.old_values[channel].x_n2,
+                    self.old_values[channel].y_n1,
+                    self.old_values[channel].y_n2
+                );
+                *output = result;
+
+                self.old_values[channel].y_n2 = self.old_values[channel].y_n1;
+                self.old_values[channel].x_n2 = self.old_values[channel].x_n1;
+                self.old_values[channel].y_n1 = result;
+                self.old_values[channel].x_n1 = sample;
             }
         }
     }
